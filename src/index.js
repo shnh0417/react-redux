0aeee24b4ef708cf68f6bcd558d1b69b0e3ff513
shnh0417 <ssung5795@gmail.com>
2022-11-21 16:39:03 +0900

    redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (count = 0, action) => {
+    if (action.type === "ADD") {
+        return count + 1;
+    } else if (action.type === "MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
+};
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+    console.log(countStore.getState());
+    number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+    countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+    countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+
+console.log(countStore);
+console.log(countStore.getState());
